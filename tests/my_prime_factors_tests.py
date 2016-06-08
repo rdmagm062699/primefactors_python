756705781960074b2e93c16fa3eaecbe60211ab0
rdmagm062699 <rob.miller@homeinsteadinc.com>
2016-06-08 14:25:56 -0500

    final changes

--- tests/my_prime_factors_tests.py
+++ tests/my_prime_factors_tests.py
@@ -4,11 +4,6 @@ import unittest
 
 class MyPrimeFactorsTest(unittest.TestCase):
 
-	def test_can_create_prime_factors_object(self):
-		pf = PrimeFactors()
-
-		nt.assert_is_not_none(pf)
-
 	def test_one_has_no_prime_factors(self):
 		pf = PrimeFactors()
 
